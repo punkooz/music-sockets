5e55b63c6f2cc6deb08cdb98aa3827dc2fe9bd5f
Samrat Das <oozsober@gmail.com>
2022-02-11 15:49:03 +0530

    basic music playing on front-end

---
+++ src/main/java/com/example/musicsockets/MusicSocketsApplication.java
@@ -0,0 +1,13 @@
+package com.example.musicsockets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicSocketsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicSocketsApplication.class, args);
+	}
+
+}
